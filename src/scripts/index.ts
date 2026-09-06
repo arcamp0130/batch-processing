03150dd95718e29337f1a21fda8ddf649370cd81
Arath Campos <arcmp0130@gmail.com>
2026-09-05 18:30:57 -0600

    feat: Using import aliases with ES for IDE and Vite.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,3 @@
-import './sass/style.scss'
+import  "@sass/style.scss";
 
 console.log("Hello from typescript!");
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
     root: "./src",
@@ -8,5 +9,13 @@ export default defineConfig({
     build: {
         outDir: './dist',
         sourcemap: true,
+    },
+    resolve: {
+        alias: {
+            "@sass": path.resolve(import.meta.dirname, './src/scripts/sass'),
+            "@managers/index": path.resolve(import.meta.dirname, './src/scripts/managers/managers.index.ts'),
+            "@managers": path.resolve(import.meta.dirname, './src/scripts/managers/'),
+            "@types": path.resolve(import.meta.dirname, './src/scripts/types')
+        }
     }
 });
