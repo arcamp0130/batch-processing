c762522b4068130721da51e71cbbdb18fe9027dd
Arath Campos <arcmp0130@gmail.com>
2026-09-06 16:53:13 -0600

    refact: using objects for lists of elemnts of the same type and compacting batch layout creation process

--- src/scripts/managers/html.manager.ts
+++ src/scripts/managers/html.manager.ts
@@ -8,23 +8,11 @@ export default class HTMLManager {
   private taskIds: number[] = [];
 
   private readonly titleDisplay: HTMLElement | null;
-  private readonly displayInput: HTMLElement | null;
-  private readonly displayProcess: HTMLElement | null;
 
-  private readonly inputUsername: HTMLInputElement | null;
-  private readonly inputProcessId: HTMLInputElement | null;
-  private readonly inputOperand1: HTMLInputElement | null;
-  private readonly inputOperation: HTMLSelectElement | null;
-  private readonly inputOperand2: HTMLInputElement | null;
-  private readonly inputEstimatedTime: HTMLInputElement | null;
-
-  private readonly buttonClear: HTMLButtonElement | null;
-  private readonly buttonAddJob: HTMLButtonElement | null;
-  private readonly buttonStart: HTMLButtonElement | null;
-
-  private readonly alertContainer: HTMLElement | null;
-  private readonly alertTitle: HTMLElement | null;
-  private readonly alertMessage: HTMLElement | null;
+  private readonly displays: { [key: string]: HTMLElement | null };
+  private readonly inputs: { [key: string]: HTMLInputElement | null };
+  private readonly buttons: { [key: string]: HTMLButtonElement | null };
+  private readonly alert: { [key: string]: HTMLElement | null };
 
   private readonly noJobsSpan: HTMLElement | null;
   private readonly previewTable: HTMLElement | null;
@@ -39,36 +27,36 @@ export default class HTMLManager {
 
   private constructor() {
     this.titleDisplay = document.querySelector("span#title-display");
-    this.displayInput = document.querySelector("div.panel div.input");
-    this.displayProcess = document.querySelector("div.panel div.process");
 
-    this.inputUsername = document.querySelector("input#username");
-    this.inputProcessId = document.querySelector("input#process-ID");
-    this.inputOperand1 = document.querySelector("input#operand1");
-    this.inputOperation = document.querySelector("select#operation-type");
-    this.inputOperand2 = document.querySelector("input#operand2");
-    this.inputEstimatedTime = document.querySelector("input#JTL");
+    this.displays = {
+      displayInput: document.querySelector("div.panel div.input"),
+      displayProcess: document.querySelector("div.panel div.process")
+    };
+    this.inputs = {
+      username: document.querySelector("input#username"),
+      processId: document.querySelector("input#process-ID"),
+      operand1: document.querySelector("input#operand1"),
+      operand2: document.querySelector("input#operand2"),
+      operation: document.querySelector("select#operation-type"),
+      estimatedTime: document.querySelector("input#JTL"),
+    };
 
-    this.buttonClear = document.querySelector("button#clear-form");
-    this.buttonAddJob = document.querySelector("button#add-job");
-    this.buttonStart = document.querySelector("button#start");
+    this.buttons = {
+      clear: document.querySelector("button#clear-form"),
+      addJob: document.querySelector("button#add-job"),
+      start: document.querySelector("button#start"),
+    };
 
-    this.alertContainer = document.querySelector("div.alert.container");
-    this.alertTitle = document.querySelector("h4#alert-title");
-    this.alertMessage = document.querySelector("p#alert-message");
+    this.alert = {
+      container: document.querySelector("div.alert.container"),
+      title: document.querySelector("h4#alert-title"),
+      message: document.querySelector("p#alert-message"),
+    };
 
     this.noJobsSpan = document.querySelector("span#no-jobs");
     this.previewTable = document.querySelector(
       "div.preview.container div.table",
     );
-  }
-
-  private batchHeader(batchId: number): HTMLElement {
-    const span: HTMLElement = document.createElement("span");
-    span.classList.add("header");
-    span.textContent = `Batch ${batchId}`;
-
-    return span;
   }
 
   private batchRecord(
@@ -97,12 +85,16 @@ export default class HTMLManager {
     return record;
   }
 
-  private batchLayout(batchId: number): HTMLElement {
+  private batchLayout(): HTMLElement {
     const batch: HTMLElement = document.createElement("div");
     batch.classList.add("batch");
-    batch.id = `#batch-${batchId}`;
+    batch.id = `#batch-${this.batchCount}`;
+    
+    const header: HTMLElement = document.createElement("span");
+    header.classList.add("header");
+    header.textContent = `Batch ${this.batchCount}`;
 
-    batch.appendChild(this.batchHeader(batchId));
+    batch.appendChild(header);
 
     return batch;
   }
