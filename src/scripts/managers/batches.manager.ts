b9d799eed05027aa68ba58343b67fbeaa0901303
Arath Campos <arcmp0130@gmail.com>
2026-09-06 19:03:06 -0600

    feat: creating and using batch and task types int batches manager

--- src/scripts/managers/batches.manager.ts
+++ src/scripts/managers/batches.manager.ts
@@ -1,6 +1,11 @@
+import { Queue } from "@structures/index";
+import { type Batch } from "../types/processing.type";
+
 export default class BatchesManager {
   private static insance: BatchesManager;
-
+  public batches: Queue<Batch> = new Queue<Batch>;
+  public currentBatch: Batch | null = null;
+  
   // singleton design pattern
   public static get Instance(): BatchesManager {
     if (!BatchesManager.insance) {
---
+++ src/scripts/types/processing.type.ts
@@ -0,0 +1,12 @@
+import type { Queue } from "@structures/index";
+import type { OperationNames } from "./math.type";
+
+export type Task = {
+  id: number;
+  operand1: number;
+  operation: OperationNames;
+  operand2: number;
+  time: number;
+};
+
+export type Batch = Queue<Task>;
